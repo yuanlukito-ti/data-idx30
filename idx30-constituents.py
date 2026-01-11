8d98989a3d4cd6c8580140cde99311158cd85090
Yuan Lukito <yuanlukito-ti@users.noreply.github.com>
2026-01-11 15:29:55 +0700

    IDX30 constituents announcement period

---
+++ idx30-constituents.py
@@ -0,0 +1,343 @@
+idx30_periods = [
+    {
+        # starting point
+        # masuk: AKRA, ASRI
+        # keluar: INCO, LSIP
+        'start': '2016-02-01',
+        'end': '2016-07-31',
+        'tickers': [
+            'ADHI', 'ADRO', 'AKRA', 'ASII', 'ASRI', 
+            'BBCA', 'BBNI', 'BBRI', 'BMRI', 'BSDE', 
+            'CPIN', 'GGRM', 'ICBP', 'INDF', 'INTP', 
+            'KLBF', 'LPKR', 'LPPF', 'MNCN', 'MPPA', 
+            'PGAS', 'PWON', 'SCMA', 'SMGR', 'SMRA', 
+            'SSMS', 'TLKM', 'UNTR', 'UNVR', 'WIKA' 
+        ]
+    },
+    {   # masuk: HMSP, WSKT
+        # keluar: MPPA, WIKA
+        'start': '2016-08-01',
+        'end': '2017-01-31',
+        'tickers': [
+            'ADHI', 'ADRO', 'AKRA', 'ASII', 'ASRI', 
+            'BBCA', 'BBNI', 'BBRI', 'BMRI', 'BSDE', 
+            'CPIN', 'GGRM', 'HMSP', 'ICBP', 'INDF', 
+            'INTP', 'KLBF', 'LPKR', 'LPPF', 'MNCN', 
+            'PGAS', 'PWON', 'SCMA', 'SMGR', 'SMRA', 
+            'SSMS', 'TLKM', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: PPRO, SRIL
+        # keluar: ASRI, SCMA
+        'start': '2017-02-01',
+        'end': '2017-07-31',
+        'tickers': [
+            'ADHI', 'ADRO', 'AKRA', 'ASII', 'BBCA', 
+            'BBNI', 'BBRI', 'BMRI', 'BSDE', 'CPIN', 
+            'GGRM', 'HMSP', 'ICBP', 'INDF', 'INTP', 
+            'KLBF', 'LPKR', 'LPPF', 'MNCN', 'PGAS', 
+            'PPRO', 'PWON', 'SMGR', 'SMRA', 'SRIL', 
+            'SSMS', 'TLKM', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: ANTM, JSMR, PTPP
+        # keluar: AKRA, CPIN, SMRA
+        'start': '2017-08-01',
+        'end': '2018-01-31',
+        'tickers': [
+            'ADHI', 'ADRO', 'ANTM', 'ASII', 'BBCA', 
+            'BBNI', 'BBRI', 'BMRI', 'BSDE', 'GGRM', 
+            'HMSP', 'ICBP', 'INDF', 'INTP', 'JSMR', 
+            'KLBF', 'LPKR', 'LPPF', 'MNCN', 'PGAS', 
+            'PPRO', 'PTPP', 'PWON', 'SMGR', 'SRIL', 
+            'SSMS', 'TLKM', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: BBTN, BMTR, BRPT
+        # keluar: ADHI, MNCN, PPRO
+        'start': '2018-02-01',
+        'end': '2018-07-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BMTR', 'BRPT', 
+            'BSDE', 'GGRM', 'HMSP', 'ICBP', 'INDF', 
+            'INTP', 'JSMR', 'KLBF', 'LPKR', 'LPPF', 
+            'PGAS', 'PTPP', 'PWON', 'SMGR', 'SRIL', 
+            'SSMS', 'TLKM', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: INKP, MEDC, PTBA, WSBP
+        # keluar: BMTR, LPKR, PWON, SSMS
+        'start': '2018-08-01',
+        'end': '2019-01-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BRPT', 'BSDE', 
+            'GGRM', 'HMSP', 'ICBP', 'INDF', 'INKP', 
+            'INTP', 'JSMR', 'KLBF', 'LPPF', 'MEDC', 
+            'PGAS', 'PTBA', 'PTPP', 'SMGR', 'SRIL', 
+            'TLKM', 'UNTR', 'UNVR', 'WSBP', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: CPIN, ITMG
+        # keluar: BSDE, WSBP
+        'start': '2019-02-01',
+        'end': '2019-07-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BRPT', 'CPIN', 
+            'GGRM', 'HMSP', 'ICBP', 'INDF', 'INKP', 
+            'INTP', 'ITMG', 'JSMR', 'KLBF', 'LPPF', 
+            'MEDC', 'PGAS', 'PTBA', 'PTPP', 'SMGR', 
+            'SRIL', 'TLKM', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: ERAA
+        # keluar: MEDC
+        'start': '2019-08-01',
+        'end': '2020-01-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BRPT', 'CPIN', 
+            'ERAA', 'GGRM', 'HMSP', 'ICBP', 'INDF', 
+            'INKP', 'INTP', 'ITMG', 'JSMR', 'KLBF', 
+            'LPPF', 'PGAS', 'PTBA', 'PTPP', 'SMGR', 
+            'SRIL', 'TLKM', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: ACES, INCO, JPFA, MNCN
+        # keluar: ITMG, JSMR, LPPF, SRIL
+        'start': '2020-02-01',
+        'end': '2020-07-31',
+        'tickers': [
+            'ACES', 'ADRO', 'ANTM', 'ASII', 'BBCA', 
+            'BBNI', 'BBRI', 'BBTN', 'BMRI', 'BRPT', 
+            'CPIN', 'ERAA', 'GGRM', 'HMSP', 'ICBP', 
+            'INCO', 'INDF', 'INKP', 'INTP', 'JPFA', 
+            'KLBF', 'MNCN', 'PGAS', 'PTBA', 'PTPP', 
+            'SMGR', 'TLKM', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # masuk: BTPS, EXCL, TOWR
+        # keluar: BRPT, PTPP, WSKT
+        'start': '2020-08-01',
+        'end': '2021-01-31',
+        'tickers': [
+            'ACES', 'ADRO', 'ANTM', 'ASII', 'BBCA', 
+            'BBNI', 'BBRI', 'BBTN', 'BMRI', 'BTPS', 
+            'CPIN', 'ERAA', 'EXCL', 'GGRM', 'HMSP', 
+            'ICBP', 'INCO', 'INDF', 'INKP', 'INTP', 
+            'JPFA', 'KLBF', 'MNCN', 'PGAS', 'PTBA', 
+            'SMGR', 'TLKM', 'TOWR', 'UNTR', 'UNVR', 
+        ]
+    },
+    {
+        # masuk: MDKA, PWON, TBIG, TKIM
+        # keluar: ACES, ERAA, INCO, JPFA
+        'start': '2021-02-01',
+        'end': '2021-07-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BTPS', 'CPIN', 
+            'EXCL', 'GGRM', 'HMSP', 'ICBP', 'INDF', 
+            'INKP', 'INTP', 'KLBF', 'MDKA', 'MNCN', 
+            'PGAS', 'PTBA', 'PWON', 'SMGR', 'TBIG', 
+            'TKIM', 'TLKM', 'TOWR', 'UNTR', 'UNVR', 
+        ]
+    },
+    {
+        # masuk: BRPT, INCO, MIKA, TINS
+        # keluar: BTPS, INTP, MNCN, PWON
+        'start': '2021-08-01',
+        'end': '2021-09-28',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BRPT', 'CPIN', 
+            'EXCL', 'GGRM', 'HMSP', 'ICBP', 'INCO', 
+            'INDF', 'INKP', 'KLBF', 'MDKA', 'MIKA', 
+            'PGAS', 'PTBA', 'SMGR', 'TBIG', 'TINS', 
+            'TKIM', 'TLKM', 'TOWR', 'UNTR', 'UNVR', 
+        ]
+    },
+    {
+        # fast entry BUKA
+        # keluar: TKIM
+        'start': '2021-09-29',
+        'end': '2022-01-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BRPT', 'BUKA', 
+            'CPIN', 'EXCL', 'GGRM', 'HMSP', 'ICBP', 
+            'INCO', 'INDF', 'INKP', 'KLBF', 'MDKA', 
+            'MIKA', 'PGAS', 'PTBA', 'SMGR', 'TBIG', 
+            'TINS', 'TLKM', 'TOWR', 'UNTR', 'UNVR', 
+        ]
+    },
+    {
+        # masuk: EMTK, WSKT
+        # keluar: GGRM, HMSP
+        'start': '2022-02-01',
+        'end': '2022-06-07',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BRPT', 'BUKA', 
+            'CPIN', 'EMTK', 'EXCL', 'ICBP', 'INCO', 
+            'INDF', 'INKP', 'KLBF', 'MDKA', 'MIKA', 
+            'PGAS', 'PTBA', 'SMGR', 'TBIG', 'TINS', 
+            'TLKM', 'TOWR', 'UNTR', 'UNVR', 'WSKT' 
+        ]
+    },
+    {
+        # fast entry GOTO
+        # keluar: WSKT
+        'start': '2022-06-08',
+        'end': '2022-07-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ASII', 'BBCA', 'BBNI', 
+            'BBRI', 'BBTN', 'BMRI', 'BRPT', 'BUKA', 
+            'CPIN', 'EMTK', 'EXCL', 'GOTO', 'ICBP', 
+            'INCO', 'INDF', 'INKP', 'KLBF', 'MDKA', 
+            'MIKA', 'PGAS', 'PTBA', 'SMGR', 'TBIG', 
+            'TINS', 'TLKM', 'TOWR', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # masuk: ARTO, HRUM, ITMG
+        # keluar: BBTN, EXCL, MIKA
+        'start': '2022-08-01',
+        'end': '2023-01-31',
+        'tickers': [
+            'ADRO', 'ANTM', 'ARTO', 'ASII', 'BBCA', 
+            'BBNI', 'BBRI', 'BMRI', 'BRPT', 'BUKA', 
+            'CPIN', 'EMTK', 'GOTO', 'HRUM', 'ICBP', 
+            'INCO', 'INDF', 'INKP', 'ITMG', 'KLBF', 
+            'MDKA', 'PGAS', 'PTBA', 'SMGR', 'TBIG', 
+            'TINS', 'TLKM', 'TOWR', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # masuk: AMRT, ESSA, MEDC
+        # keluar: ICBP, INKP, TINS
+        'start': '2023-02-01',
+        'end': '2023-07-31',
+        'tickers': [
+            'ADRO', 'AMRT', 'ANTM', 'ARTO', 'ASII', 
+            'BBCA', 'BBNI', 'BBRI', 'BMRI', 'BRPT', 
+            'BUKA', 'CPIN', 'EMTK', 'ESSA', 'GOTO', 
+            'HRUM', 'INCO', 'INDF', 'ITMG', 'KLBF', 
+            'MDKA', 'MEDC', 'PGAS', 'PTBA', 'SMGR', 
+            'TBIG', 'TLKM', 'TOWR', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # masuk: AKRA
+        # keluar: TBIG
+        'start': '2023-08-01',
+        'end': '2024-01-31',
+        'tickers': [
+            'ADRO', 'AKRA', 'AMRT', 'ANTM', 'ARTO', 
+            'ASII', 'BBCA', 'BBNI', 'BBRI', 'BMRI', 
+            'BRPT', 'BUKA', 'CPIN', 'EMTK', 'ESSA', 
+            'GOTO', 'HRUM', 'INCO', 'INDF', 'ITMG', 
+            'KLBF', 'MDKA', 'MEDC', 'PGAS', 'PTBA', 
+            'SMGR', 'TLKM', 'TOWR', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # masuk: ACES, ICBP, INKP, PGEO
+        # keluar: EMTK, ESSA, HRUM, TOWR
+        'start': '2024-02-01',
+        'end': '2024-07-31',
+        'tickers': [
+            'ACES', 'ADRO', 'AKRA', 'AMRT', 'ANTM', 
+            'ARTO', 'ASII', 'BBCA', 'BBNI', 'BBRI', 
+            'BMRI', 'BRPT', 'BUKA', 'CPIN', 'GOTO', 
+            'ICBP', 'INCO', 'INDF', 'INKP', 'ITMG', 
+            'KLBF', 'MDKA', 'MEDC', 'PGAS', 'PGEO', 
+            'PTBA', 'SMGR', 'TLKM', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # Update IDX 30 setiap 3 bulan mulai 2024
+        # masuk: -
+        # keluar: -
+        'start': '2024-08-01',
+        'end': '2024-10-31',
+        'tickers': [
+            'ACES', 'ADRO', 'AKRA', 'AMRT', 'ANTM', 
+            'ARTO', 'ASII', 'BBCA', 'BBNI', 'BBRI', 
+            'BMRI', 'BRPT', 'BUKA', 'CPIN', 'GOTO', 
+            'ICBP', 'INCO', 'INDF', 'INKP', 'ITMG', 
+            'KLBF', 'MDKA', 'MEDC', 'PGAS', 'PGEO', 
+            'PTBA', 'SMGR', 'TLKM', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # Update IDX 30 setiap 3 bulan mulai 2024
+        # masuk: MAPI, MBMA
+        # keluar: BUKA, ITMG
+        'start': '2024-11-01',
+        'end': '2025-01-31',
+        'tickers': [
+            'ACES', 'ADRO', 'AKRA', 'AMRT', 'ANTM', 
+            'ARTO', 'ASII', 'BBCA', 'BBNI', 'BBRI', 
+            'BMRI', 'BRPT', 'CPIN', 'GOTO', 'ICBP', 
+            'INCO', 'INDF', 'INKP', 'KLBF', 'MAPI', 
+            'MBMA', 'MDKA', 'MEDC', 'PGAS', 'PGEO', 
+            'PTBA', 'SMGR', 'TLKM', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # Update IDX 30 setiap 3 bulan mulai 2024
+        # masuk: EXCL, ISAT
+        # keluar: ACES, PGEO
+        'start': '2025-02-03',
+        'end': '2025-04-30',
+        'tickers': [
+            'ADRO', 'AKRA', 'AMRT', 'ANTM', 'ARTO', 
+            'ASII', 'BBCA', 'BBNI', 'BBRI', 'BMRI', 
+            'BRPT', 'CPIN', 'EXCL', 'GOTO', 'ICBP', 
+            'INCO', 'INDF', 'INKP', 'ISAT', 'KLBF', 
+            'MAPI', 'MBMA', 'MDKA', 'MEDC', 'PGAS', 
+            'PTBA', 'SMGR', 'TLKM', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # Update IDX 30 setiap 3 bulan mulai 2024
+        # masuk: BBTN
+        # keluar: ARTO
+        'start': '2025-05-02',
+        'end': '2025-07-31',
+        'tickers': [
+            'ADRO', 'AKRA', 'AMRT', 'ANTM', 'ASII', 
+            'BBCA', 'BBNI', 'BBRI', 'BBTN', 'BMRI', 
+            'BRPT', 'CPIN', 'EXCL', 'GOTO', 'ICBP', 
+            'INCO', 'INDF', 'INKP', 'ISAT', 'KLBF', 
+            'MAPI', 'MBMA', 'MDKA', 'MEDC', 'PGAS', 
+            'PTBA', 'SMGR', 'TLKM', 'UNTR', 'UNVR' 
+        ]
+    },
+    {
+        # Update IDX 30 setiap 3 bulan mulai 2024
+        # masuk: ITMG, JPFA
+        # keluar: BBTN, MAPI
+        'start': '2025-08-01',
+        'end': '2025-10-31',
+        'tickers': [
+            'ADRO', 'AKRA', 'AMRT', 'ANTM', 'ASII', 
+            'BBCA', 'BBNI', 'BBRI', 'BMRI', 'BRPT', 
+            'CPIN', 'EXCL', 'GOTO', 'ICBP', 'INCO', 
+            'INDF', 'INKP', 'ISAT', 'ITMG', 'JPFA', 
+            'KLBF', 'MBMA', 'MDKA', 'MEDC', 'PGAS', 
+            'PTBA', 'SMGR', 'TLKM', 'UNTR', 'UNVR' 
+        ]
+    }
+]
